370b5e4e133576e3f71f21ec2b8a26d32199c457
dersanli <dersanli@gmail.com>
2017-02-11 16:07:39 +0300

    Problem 20 added.

---
+++ Problem019/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Problem019
+{
+    /*
+        You are given the following information, but you may prefer to do some research for yourself.
+
+            1 Jan 1900 was a Monday.
+            Thirty days has September,
+            April, June and November.
+            All the rest have thirty-one,
+            Saving February alone,
+            Which has twenty-eight, rain or shine.
+            And on leap years, twenty-nine.
+            A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+
+        How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+    */
+
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            DateTime Start = DateTime.Now;
+
+            DateTime ProblemStart = new DateTime (1901, 1, 1);
+            DateTime ProblemEnd = new DateTime (2000, 12, 31);
+
+            uint SundayCount = 0;
+
+            for (DateTime i = ProblemStart; i <= ProblemEnd; i = i.AddDays (1)) {
+                if (i.Day == 1) {
+                    if (i.DayOfWeek == DayOfWeek.Sunday) {
+                        SundayCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine ("Sunday Count: {0}", SundayCount);
+            Console.WriteLine ("\nElapsed: {0}", DateTime.Now - Start);
+        }
+    }
+}
---
+++ Problem020/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem020
+{
+    /*
+        n! means n × (n − 1) × ... × 3 × 2 × 1
+
+        For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+        and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+        Find the sum of the digits in the number 100!
+    */
+
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            DateTime Start = DateTime.Now;
+
+            BigInteger Number = 100;
+            BigInteger Factorial = 0;
+
+            Factorial = FactorialOf (Number);
+
+            Console.WriteLine ("{0,4}! = {1,16}", Number, Factorial);
+
+            string FactorialString = Factorial.ToString ();
+
+            uint FactorialSum = 0;
+            uint Current = 0;
+
+            foreach (var item in FactorialString) {
+                Current = uint.Parse (item.ToString ());
+                if (0 != Current)
+                    FactorialSum += Current;
+            }
+
+            Console.WriteLine ("Factorial Sum: {0}", FactorialSum);
+
+            Console.WriteLine ("\nElapsed: {0}", DateTime.Now - Start);
+        }
+
+        static BigInteger FactorialOf(BigInteger Number)
+        {
+            if (Number >= 1)
+                return Number * FactorialOf (Number - 1);
+            else
+                return 1;
+        }
+    }
+}
