c42263e9a7a700ad9691feb60d56ace21fa399bc
dersanli <dersanli@gmail.com>
2017-02-13 16:19:17 +0300

    Project 22 added.

---
+++ Problem022/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace Problem022
+{
+	class MainClass
+	{
+		static SortedList Names = new SortedList ();
+
+		public static void Main (string[] args)
+		{
+			DateTime Start = DateTime.Now;
+
+			BuildNamesList ();
+			IterateNames ();
+
+			Console.WriteLine("Elapsed: {0}", DateTime.Now - Start);
+
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+
+		static void IterateNames()
+		{
+			ulong SumScores = 0;
+			for (int i = 0; i < Names.Count; i++) {
+				SumScores += (ulong)((int)Names.GetByIndex (i) * (i + 1));
+			}
+
+			Console.WriteLine ("Sum of the Scores: {0}", SumScores);
+		}
+
+
+		static int GetNameScore(string Name)
+		{
+			int Score = 0;
+
+			foreach (var Letter in Name) {
+				Score += ((int)Letter) - 64;
+			}
+			
+			return Score;
+		}
+
+		static void BuildNamesList()
+		{
+			string[] Values;
+
+			try
+			{
+				using (StreamReader sr = new StreamReader("p022_names.txt"))
+				{
+					string line;
+					while((line = sr.ReadLine()) != null)  
+					{  
+						Values = line.Split(',');
+						string CurrentName = string.Empty;
+						int CurrentNameScore = 0;
+
+						for (int i = 0; i < Values.Length; i++) {
+							CurrentName = Values[i].Replace("\"", string.Empty);
+							CurrentNameScore = GetNameScore(CurrentName);
+
+							Names.Add(CurrentName, CurrentNameScore );
+						}
+					}  
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine ("Exception: {0}", ex.Message);
+			}
+		}
+	}
+}
