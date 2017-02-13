30f3685d3d813193a3b449dfc16a8609baffe66a
dersanli <dersanli@gmail.com>
2017-02-13 10:36:19 +0300

    Problem 21 solved.

--- Problem021/Program.cs
+++ Problem021/Program.cs
@@ -1,61 +1,70 @@
-﻿/*
- * Created by SharpDevelop.
- * User: dersanli
- * Date: 12-Feb-17
- * Time: 4:58 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace Problem021
 {
-	class Program
+	class MainClass
 	{	
-		static Dictionary<ulong,ulong> Numbers = new Dictionary<ulong,ulong>();
-		static Dictionary<ulong,ulong> Pairs = new Dictionary<ulong,ulong>();
-		
+		static List<int> Numbers = new List<int> ();
+		static List<int> Values = new List<int> ();
+
 		public static void Main(string[] args)
 		{
 			DateTime Start = DateTime.Now;
 
-			for (ulong i = 10000; i > 0; i--) {
-				
-				Numbers.Add(i, AddFactors(i));
-	
+			int Total = 0;
+
+			for (int i = 10000; i > 0; i--) {
+
+				if(AddFactors(i) < 10000){
+					Numbers.Add (i);
+					Values.Add (AddFactors (i));
+				}
 			}
-			
-			foreach (var key in Numbers) {
-				
-				foreach (var value in Numbers) {
-					if(key.Key == 1 || value.Value ==1)
-						continue;
-					
-					if (key.Value == value.Value)
-					{
-						Pairs.Add( key.Key, value.Key);
-						Console.WriteLine("{0} - {1}", key.Key, value.Key);
+
+			for (int NumberIndex = 0; NumberIndex < Numbers.Count; NumberIndex++) {
+
+				int CurrentNumber = Numbers [NumberIndex];
+				int CurrentValue = Values [NumberIndex];
+
+				int CandidateNumber = 0;
+				int CandidateValue = 0;
+
+				if (CurrentValue == 1)
+					continue;
+
+				int CandidateIndex = Numbers.IndexOf (CurrentValue);
+
+				if (CandidateIndex < 0)
+					continue;
+
+				CandidateNumber = Numbers [CandidateIndex];
+				CandidateValue = Values [CandidateIndex];
+
+				if (CandidateNumber != CurrentNumber) {
+					if (CandidateValue == CurrentNumber) {
+						Total += CurrentNumber;
 					}
 				}
 			}
 
+			Console.WriteLine ("The Total is: {0}", Total);
 			Console.WriteLine("Elapsed: {0}", DateTime.Now - Start);
-			Console.Write("Press any key to continue . . . ");
+			Console.WriteLine("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
 		
-		static ulong AddFactors(ulong Number)
+		static int AddFactors(int Number)
 		{
-			Console.Write("Number: {0,10}: ", Number);
+			//Console.Write("Number: {0,10}: ", Number);
 
-			ulong SumFactors = 0;
-			ulong max = (ulong)Math.Sqrt(Number);  //round down
-			for (ulong factor = 1; factor <= max; ++factor) { //test from 1 to the square root, or the int below it, inclusive.
+			int SumFactors = 0;
+			int max = (int)Math.Sqrt(Number);
+			for (int factor = 1; factor <= max; ++factor) {
 				if (Number % factor == 0) {
 					SumFactors += factor;
 //					Console.Write("{0} ", factor);
-					if (factor != Number / factor) { // Don't add the square root twice!  Thanks Jon
+					if (factor != Number / factor) {
 						if (Number != Number / factor) {
 							SumFactors += Number / factor;
 //							Console.Write("{0} ", Number / factor);
@@ -63,7 +72,7 @@ namespace Problem021
 					}
 				}
 			}
-			Console.Write("({0, 6})\n", SumFactors);
+			//Console.Write("({0, 6})\n", SumFactors);
 			return SumFactors;
 		}
 	}
