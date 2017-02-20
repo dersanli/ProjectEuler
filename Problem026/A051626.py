47da58240d2fe99ec29451567d09a5a91a018687
dersanli <dersanli@gmail.com>
2017-02-20 11:43:09 +0300

    Project 26 solved.

---
+++ Problem026/A051626.py
@@ -0,0 +1,18 @@
+﻿def A051626(n):
+
+#    if isA003592(n):
+#
+#        return 0
+#
+#    else:
+
+	lpow=1
+
+	while True:
+
+	    for mpow in range(lpow-1, -1, -1):
+
+	        if (10**lpow-10**mpow) % n == 0:
+
+	            return lpow-mpow
+	    lpow += 1
---
+++ Problem026/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem026
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			DateTime Start = DateTime.Now;
+
+			int MaxRecDigits = 0;
+
+			for (int Number = 1; Number <= 1000; Number++) {
+				int RecDigits = FindRecurringDigitsLength (Number);
+				Console.WriteLine("Number: {0} - RecLength: {1}", Number, RecDigits );
+
+				if (RecDigits > MaxRecDigits)
+					MaxRecDigits = RecDigits;
+			}
+
+
+			Console.WriteLine("Maximum : {0}", MaxRecDigits);
+			Console.WriteLine("Elapsed: {0}", DateTime.Now - Start);
+			System.Media.SystemSounds.Beep.Play();
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+
+
+		static int FindRecurringDigitsLength( int Denominator )
+		{
+			int lpow = 1;
+
+			do {
+				for (int mpow = lpow - 1; mpow > -1; mpow--) {
+					
+					BigInteger plpow = BigInteger.Pow(10,lpow);
+					BigInteger pmpow = BigInteger.Pow(10,mpow);
+
+					BigInteger diff = plpow - pmpow;
+					BigInteger denom = (BigInteger) Denominator;
+
+					BigInteger k = new BigInteger(0);
+					BigInteger.DivRem(diff, denom, out k);
+
+					if(k.IsZero){
+						return lpow - mpow;
+					}
+
+				}
+				lpow++;
+			} while (true);
+		}
+	}
+}
